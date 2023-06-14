fa76777f106d67110a38ded8d6571e201b6e57c5
Yordan Strahinov <ystrahinov@schonfeld.com>
2023-06-14 11:51:04 +0000

    ASG with subnet references from VPC data source

--- code/terraform-YS/02-intro-to-terraform-syntax/webserver-cluster/main.tf
+++ code/terraform-YS/02-intro-to-terraform-syntax/webserver-cluster/main.tf
@@ -35,6 +35,7 @@ resource "aws_launch_configuration" "example" {
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnets.default.ids
 
   min_size = 2
   max_size = 10
@@ -43,6 +44,17 @@ resource "aws_autoscaling_group" "example" {
     key = "Name"
     value = "terraform-asg-example"
     propagate_at_launch = true
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
 }
 
@@ -57,4 +69,3 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
