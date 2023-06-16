97d522ac12ae93da9c8fe064b8b25b829156a89b
Yordan Strahinov <ystrahinov@schonfeld.com>
2023-06-16 09:40:30 +0000

    02-aws_lb

--- code/terraform-YS/02-intro-to-terraform-syntax/webserver-cluster/main.tf
+++ code/terraform-YS/02-intro-to-terraform-syntax/webserver-cluster/main.tf
@@ -12,8 +12,8 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 
-  access_key = "AKIAU4ALRGXA24CQAUXM"
-  secret_key = "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"
+  access_key = "AKIA5HNPASA4TCELORHS"
+  secret_key = "5NAnA/noMR7nrRwFsbvV6i8xtqhiHo0UvQMixJEn"
 }
 
 resource "aws_launch_configuration" "example" {
@@ -34,8 +34,11 @@ resource "aws_launch_configuration" "example" {
 }
 
 resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier = data.aws_subnets.default.ids
+  launch_configuration  = aws_launch_configuration.example.name
+  vpc_zone_identifier   = data.aws_subnets.default.ids
+
+  target_group_arns     = [aws_lb_target_group.asg.arn]
+  health_check_type     = "ELB"
 
   min_size = 2
   max_size = 10
@@ -59,19 +62,86 @@ data "aws_subnets" "default" {
 }
 
 resource "aws_lb" "example" {
-  name = "terraform-asg-example"
-  load_balancer_type = "application"
-  subnets = data.aws_subnets.default.ids
+  name                  = "terraform-asg-example"
+  load_balancer_type    = "application"
+  subnets               = data.aws_subnets.default.ids
+  security_groups       = [aws_security_group.alb.id]
 }
 
-resource "aws_security_group" "instance" {
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
 
-  name = var.security_group_name
+  #By default, return a simpe 403
 
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  # Allow inbound HTTP requests
   ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  
+  }
+
+  #Allow all outbound requests
+  egress ={
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name = "terraform-asg-example"
+  port = var.server_port
+  
+}
+
+resource "aws_lb_target_group" "asg" {
+  name          = "terraform-asg-example"
+  port          = var.server_port
+  protocol      = "HTTP"
+  vpc_id        = data.aws_subnets.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }     
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn          = aws_lb_listener.http.arn
+  priority              = 100
+
+  condition {
+    path_pattern {
+      values            = ["*"]
+    }
+  }
+
+  action {
+    type                = "forward"
+    target_group_arn    = aws_lb_target_group.asg.arn
   }
 }
