218c6b6c94d752a3da5f4e6bab9b205bfd049838
Yordan Strahinov <ystrahinov@schonfeld.com>
2023-09-28 14:19:54 +0000

    F

---
+++ code/terraform-YS/EFS/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  access_key = "AKIAU4ALRGXA24CQAUXM"
+  secret_key = "diXOoPYuPEBSUqfOntxI7QPCOcjqZmLPgzAG0h77"
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-022e1a32d3f742bd8"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+
+  name = var.security_group_name
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
