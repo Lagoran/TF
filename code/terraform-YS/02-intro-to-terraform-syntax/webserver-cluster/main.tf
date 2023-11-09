5827d8463caed8b3da9642a75d1e2c576e60c11a
Yordan Strahinov <ystrahinov@schonfeld.com>
2023-11-09 13:26:49 +0000

    TF-YS-02 webserver-cluster tweaks

--- code/terraform-YS/02-intro-to-terraform-syntax/webserver-cluster/main.tf
+++ code/terraform-YS/02-intro-to-terraform-syntax/webserver-cluster/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 }
@@ -12,8 +12,8 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 
-  access_key = "AKIAVFSEQVHZY5Y5KDEM"
-  secret_key = "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"
+  access_key = "AKIAQ5WABLEVABZNKHT4"
+  secret_key = "3aXHkHbwkp1D6zudM0pB8yZHRdzatgZW4NVGcroH"
 }
 
 resource "aws_launch_configuration" "example" {
@@ -29,16 +29,16 @@ resource "aws_launch_configuration" "example" {
 
   # Required when using a launch configuration with an auto scaling group
   lifecycle {
-    create_before_destroy = true
+    create_before_destroy = true                                        #Every Terraform resource supports several lifecycle settings that configure how that resource is created, updated, and/or deleted. A particularly useful lifecycle setting is create_before_destroy. If you set create_before_destroy to true, Terraform will invert the order in which it replaces resources, creating the replacement resource first (including updating any references that were pointing at the old resource to point to the replacement) and then deleting the old resource. 
   }
 }
 
 resource "aws_autoscaling_group" "example" {
-  launch_configuration  = aws_launch_configuration.example.name
+  launch_configuration  = aws_launch_configuration.example.name         #Note that the ASG uses a reference to fill in the launch configuration name. This leads to a problem: launch configurations are immutable, so if you change any parameter of your launch configuration, Terraform will try to replace it. Normally, when replacing a resource, Terraform would delete the old resource first and then creates its replacement, but because your ASG now has a reference to the old resource, Terraform won’t be able to delete it.
   vpc_zone_identifier   = data.aws_subnets.default.ids
 
-  target_group_arns     = [aws_lb_target_group.asg.arn]
-  health_check_type     = "ELB"
+  target_group_arns     = [aws_lb_target_group.asg.arn]                 #How does the target group know which EC2 Instances to send requests to? You could attach a static list of EC2 Instances to the target group using the aws_lb_target_group_attachment resource, but with an ASG, Instances can launch or terminate at any time, so a static list won’t work. Instead, you can take advantage of the first-class integration between the ASG and the ALB. Go back to the aws​_autoscaling_group resource, and set its target_group_arns argument to point at your new target group
+  health_check_type     = "ELB"                                         #You should also update the health_check_type to "ELB". The default health_check_type is "EC2", which is a minimal health check that considers an Instance unhealthy only if the AWS hypervisor says the VM is completely down or unreachable. The "ELB" health check is more robust, because it instructs the ASG to use the target group’s health check to determine whether an Instance is healthy and to automatically replace Instances if the target group reports them as unhealthy. That way, Instances will be replaced not only if they are completely down but also if, for example, they’ve stopped serving requests because they ran out of memory or a critical process crashed.
 
   min_size = 2
   max_size = 10
@@ -61,17 +61,7 @@ resource "aws_security_group" "instance" {
   }
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name    = "vpc-id"
-    values  = [data.aws_vpc.default.id]
-  }
-}
-
+#Deploying a Load Balancer
 resource "aws_lb" "example" {
   name                  = var.alb_name
   load_balancer_type    = "application"
@@ -79,6 +69,8 @@ resource "aws_lb" "example" {
   security_groups       = [aws_security_group.alb.id]
 }
 
+
+#ALB part1 - Listener - Listens on a specific port (e.g., 80) and protocol (e.g., HTTP).
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
   port              = 80
@@ -117,6 +109,9 @@ resource "aws_security_group" "alb" {
   }
 }
 
+
+#ALB part 3 - Target groups  One or more servers that receive requests from the load balancer. The target group also performs health checks on these servers and sends requests only to healthy nodes.
+
 resource "aws_lb_target_group" "asg" {
 
   name = var.alb_name
@@ -135,6 +130,9 @@ resource "aws_lb_target_group" "asg" {
     unhealthy_threshold = 2
   }
 }
+
+
+#ALB part 2 - Listener rule - The following code adds a listener rule that sends requests that match any path to the target group that contains your ASG.
 
 resource "aws_lb_listener_rule" "asg" {
   listener_arn          = aws_lb_listener.http.arn
