1d87c70b2947b6ac8a6640305a4fe93014a8afa3
Yordan Strahinov <ystrahinov@schonfeld.com>
2023-11-09 11:49:25 +0000

    TF-YS-02 Change output variable

--- code/terraform-YS/02-intro-to-terraform-syntax/one-webserver-with-vars/outputs.tf
+++ code/terraform-YS/02-intro-to-terraform-syntax/one-webserver-with-vars/outputs.tf
@@ -1,4 +1,4 @@
-output "public_ip" {
-  value       = aws_instance.example.public_ip
+output "EC2_All_Attribute_Reference" {
+  value       = aws_instance.example.*              #Adding all "Attribute refenrences according to documentation - https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance"
   description = "The public IP address of the web server"
 }
